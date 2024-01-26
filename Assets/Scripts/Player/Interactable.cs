817f549035ededd1383ec78a395425b162df1347
Sami Laukkanen <sami.laukkanen@dc.lab>
2024-01-27 01:19:53 +0200

    Interaction System Events & Lerped Interactable Movement

--- Assets/Scripts/Player/Interactable.cs
+++ Assets/Scripts/Player/Interactable.cs
@@ -11,16 +11,28 @@ public class Interactable : MonoBehaviour
     [SerializeField] private UnityEvent<Interactable, Interactor> onHoverStart; public UnityEvent<Interactable, Interactor> OnHoverStart { get { return onHoverStart; } }
     [SerializeField] private UnityEvent<Interactable, Interactor> onHoverEnd; public UnityEvent<Interactable, Interactor> OnHoverEnd { get { return onHoverEnd; } }
 
+    private Transform originalParent;
     private bool interacting;
     private bool hovering;
+
+    void Awake()
+    {
+        Initialize();
+    }
+
+    void Initialize()
+    {
+        originalParent = transform.parent;
+    }
 
     public void HoverStart(Interactor interactor)
     {
         if (hovering)
             return;
 
-        hovering = true;
         Debug.Log("Interactable | " + gameObject.name + ": Hover Start");
+
+        hovering = true;
         onHoverStart.Invoke(this, interactor);
     }
 
@@ -29,18 +41,21 @@ public class Interactable : MonoBehaviour
         if (!hovering)
             return;
 
-        hovering = false;
         Debug.Log("Interactable | " + gameObject.name + ": Hover End");
+
+        hovering = false;
         onHoverEnd.Invoke(this, interactor);
     }
 
-    public void InteractStart(Interactor interactor)
+    public void InteractStart(Interactor interactor, Transform parent = null)
     {
         if (interacting)
             return;
-            
-        interacting = true;
+
         Debug.Log("Interactable | " + gameObject.name + ": Interact Start");
+
+        interacting = true;
+        ParentInteractable(parent);
         onInteractStart.Invoke(this, interactor);
     }
 
@@ -49,8 +64,21 @@ public class Interactable : MonoBehaviour
         if (!interacting)
             return;
 
-        interacting = false;
         Debug.Log("Interactable | " + gameObject.name + ": Interact End");
+
+        interacting = false;
+        ParentInteractable(originalParent);
         onInteractEnd.Invoke(this, interactor);
+    }
+
+    void ParentInteractable(Transform parent)
+    {
+        transform.SetParent(parent);
+
+        if (parent != null)
+        {
+            transform.localPosition = Vector3.zero;
+            transform.localRotation = Quaternion.identity;
+        }
     }
 }
