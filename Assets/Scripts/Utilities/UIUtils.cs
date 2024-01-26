b82391107c063d2890f8d1973c02e0451354ae62
Oskari Rautala <oskari.rautala@dc.lab>
2024-01-26 20:51:13 +0200

    Some basic utility scripts and GameData scriptable object extension.

---
+++ Assets/Scripts/Utilities/UIUtils.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextUIPool : GenericUIPool<TextMeshProUGUI> { }
+
+public class GenericUIPool<T> where T : MonoBehaviour
+{
+
+    private T defaultItem;
+    private Transform elementParent;
+    private List<T> pooledItems = new List<T>();
+
+    public List<T> ElementsInList => new List<T>(this.pooledItems);
+    public List<T> ActiveElementsInList => new List<T>(this.pooledItems).FindAll(x => x.gameObject.activeSelf);
+
+    public void SetupPool(T firstItem, Transform parent)
+    {
+        this.pooledItems.Add(firstItem);
+        defaultItem = firstItem;
+        this.elementParent = parent;
+        ResetPool();
+    }
+
+    public void ResetPool()
+    {
+        foreach (T item in this.pooledItems) { item.gameObject.SetActive(false); }
+    }
+    
+    public T GetNextItem()
+    {
+        T elementToGet = this.pooledItems.Find(x => x.gameObject.activeSelf == false);
+
+        if (elementToGet == null)
+        {
+            elementToGet = GameObject.Instantiate(this.defaultItem.gameObject, this.elementParent).GetComponent<T>();
+            this.pooledItems.Add(elementToGet);
+        }
+
+        elementToGet.gameObject.SetActive(true);
+        return elementToGet;
+    }
+
+    public void ReturnToPool(T itemToReturn)
+    {
+        itemToReturn.gameObject.SetActive(false);
+    }
+
+}
