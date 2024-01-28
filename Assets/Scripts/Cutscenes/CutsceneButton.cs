1abd241f1fd10655755761383570704ce9e806fe
LekaKeisari <68687079+LekaKeisari@users.noreply.github.com>
2024-01-28 12:43:04 +0200

    Fixed Cutscene button

--- Assets/Scripts/Cutscenes/CutsceneButton.cs
+++ Assets/Scripts/Cutscenes/CutsceneButton.cs
@@ -3,21 +3,33 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using System.Linq;
+using System;
+using UnityEngine.Events;
 
 public class CutsceneButton : MonoBehaviour
 {
     private Button button;
+    private UnityAction loadStartScene;
 
     private void Start()
     {
         button = GetComponent<Button>();
         button.onClick.AddListener(CutsceneManager.Instance.ReturnToGame);
-        if (SceneManager.GetActiveScene().name == "DayComplete")
-            button.onClick.AddListener(GameManager.Instance.StartNewRound);
+        
 
+        Scene[] scenes = SceneManager.GetAllScenes();
 
+        foreach (Scene scene in scenes)
+        {
+            if (scene.name == "DayComplete")
+                button.onClick.AddListener(GameManager.Instance.StartNewRound);
 
-        if (SceneManager.GetActiveScene().name == "End_Loss" || SceneManager.GetActiveScene().name == "End_Win")
-            SceneManager.LoadScene("StartScene");
+            if (scene.name == "End_Loss" || scene.name == "End_Win")
+            {
+                loadStartScene = delegate { SceneManager.LoadScene("StartScene"); };
+                button.onClick.AddListener(loadStartScene);
+            }
+        }
     }
 }
